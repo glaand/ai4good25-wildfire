afdffc903929ab14f10dfb1ab24ab0715a8279d3
André Glatzl <andreglatzl@gmail.com>
2025-10-16 15:41:09 +0200

    Created a cfg configuration file

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -4,3 +4,4 @@ from .LogisticRegression import LogisticRegression
 from .SMPModel import SMPModel
 from .UTAELightning import UTAELightning
 from .EvaluationMetrics import MyTestMetrics
+from .UTAEGaussian import UTAEGaussian
